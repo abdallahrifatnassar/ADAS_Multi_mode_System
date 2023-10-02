d176acfb28031d29ff70475f7fdf6469d66de253
a.rifatnassar@gmail.com <a.rifatnassar@gmail.com>
2023-10-02 22:43:41 +0300

    added channel name

---
+++ include/MCAL/MUART_config.h
@@ -0,0 +1,21 @@
+/**************************************/
+/* Author    : Ahmed Elyousfy         */ 
+/* SWC       : UART	                  */
+/* Layer     : MCAL	                  */
+/* Version   : 1.0                    */
+/* Date      : 0ctober 02 ,2023	      */
+/* Last Edit : N/A                    */
+/**************************************/
+
+#ifndef MUART_CONFIG_H_
+#define MUART_CONFIG_H_
+
+#define UART_BAUD_RATE	9600
+
+/* 
+FOR 2 STOP BITS SET VALUE TO 2
+FOR 1 STOP BITS SET VALUE TO 0
+*/
+#define STOP_BIT_VALUE	2
+
+#endif
