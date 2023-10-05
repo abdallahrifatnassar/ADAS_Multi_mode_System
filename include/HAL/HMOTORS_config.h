f24e5e3ea8f0249d41ac97733dd1c159bbbae1fe
Ahmed Samir <Ahmedsamirsalm@gmail.com>
2023-10-05 11:03:32 +0300

    Motor driver is done

--- include/HAL/HMOTORS_config.h
+++ include/HAL/HMOTORS_config.h
@@ -10,23 +10,24 @@
 #ifndef _HMOTOR_CONFIG_H_
 #define _HMOTOR_CONFIG_H_
 
-/*Motor 1 Direction Pins*/
-#define MOTOR_IN1_PORT			/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC*/
-#define MOTOR_IN1_PIN			/*MGPIO_PIN(0->15)Except Portc*/
 
-#define MOTOR_IN2_PORT			/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC*/
-#define MOTOR_IN2_PIN			/*MGPIO_PIN(0->15)Except Portc*/
+/************************/
+/*Motor 1 Direction Pins Write it in Pair (PORT,PIN)*/
+#define MOTOR_IN1_PORT_PIN					/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC,MGPIO_PIN(0->15)Except Portc*/
+
+#define MOTOR_IN2_PORT_PIN					/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC,MGPIO_PIN(0->15)Except Portc*/
 
 /************************/
 
 
-/*Motor 2 Direction Pins*/
-#define MOTOR_IN3_PORT			/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC*/
-#define MOTOR_IN3_PIN			/*MGPIO_PIN(0->15)Except Portc*/
+/*Motor 2 Direction Pins Write it in Pair (PORT,PIN)*/
+#define MOTOR_IN3_PORT_PIN					/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC,MGPIO_PIN(0->15)Except Portc*/
 
-#define MOTOR_IN4_PORT			/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC*/
-#define MOTOR_IN4_PIN			/*MGPIO_PIN(0->15)Except Portc*/
+#define MOTOR_IN4_PORT_PIN					/*MGPIO_PORTA,MGPIO_PORTB,MGPIO_PORTC,MGPIO_PIN(0->15)Except Portc*/
 
 /************************/
+/*Motors Speed Pins in Pair(PORT,PIN),Note that it Must be one of Timer3 Channels
+(PB0,PB1,PB4,PB5,PA6,PA7) */
+#define MOTOR_EN_PORT_PIN
 
 #endif
--- source/HAL/HMOTORS_program.c
+++ source/HAL/HMOTORS_program.c
@@ -20,50 +20,89 @@ void HMOTOR_voidInit(void)
 {
 	/*Set Motor control Direction pins to output Pushpull (IN1->4) */
 	
-	MGPIO_voidSetPinMode(MOTOR_IN1_PORT,MOTOR_IN1_PIN,MGPIO_PIN_OUPUT);
-	MGPIO_voidSetOutputPinType(MOTOR_IN1_PORT,MOTOR_IN1_PIN,MGPIO_PIN_PUSH_PULL);
-	MGPIO_voidSetOutputPinSpeed(MOTOR_IN1_PORT,MOTOR_IN1_PIN,MGPIO_MEDIUMSPEED);
+	MGPIO_voidSetPinMode(MOTOR_IN1_PORT_PIN,MGPIO_PIN_OUPUT);
+	MGPIO_voidSetOutputPinType(MOTOR_IN1_PORT_PIN,MGPIO_PIN_PUSH_PULL);
+	MGPIO_voidSetOutputPinSpeed(MOTOR_IN1_PORT_PIN,MGPIO_MEDIUMSPEED);
 													 
-	MGPIO_voidSetPinMode(MOTOR_IN2_PORT,MOTOR_IN2_PIN,MGPIO_PIN_OUPUT);
-	MGPIO_voidSetOutputPinType(MOTOR_IN2_PORT,MOTOR_IN2_PIN,MGPIO_PIN_PUSH_PULL);
-	MGPIO_voidSetOutputPinSpeed(MOTOR_IN2_PORT,MOTOR_IN2_PIN,MGPIO_MEDIUMSPEED);
+	MGPIO_voidSetPinMode(MOTOR_IN2_PORT_PIN,MGPIO_PIN_OUPUT);
+	MGPIO_voidSetOutputPinType(MOTOR_IN2_PORT_PIN,MGPIO_PIN_PUSH_PULL);
+	MGPIO_voidSetOutputPinSpeed(MOTOR_IN2_PORT_PIN,MGPIO_MEDIUMSPEED);
 													 
-	MGPIO_voidSetPinMode(MOTOR_IN3_PORT,MOTOR_IN3_PIN,MGPIO_PIN_OUPUT);
-	MGPIO_voidSetOutputPinType(MOTOR_IN3_PORT,MOTOR_IN3_PIN,MGPIO_PIN_PUSH_PULL);
-	MGPIO_voidSetOutputPinSpeed(MOTOR_IN3_PORT,MOTOR_IN3_PIN,MGPIO_MEDIUMSPEED);
+	MGPIO_voidSetPinMode(MOTOR_IN3_PORT_PIN,MGPIO_PIN_OUPUT);
+	MGPIO_voidSetOutputPinType(MOTOR_IN3_PORT_PIN,MGPIO_PIN_PUSH_PULL);
+	MGPIO_voidSetOutputPinSpeed(MOTOR_IN3_PORT_PIN,MGPIO_MEDIUMSPEED);
 													 
-	MGPIO_voidSetPinMode(MOTOR_IN4_PORT,MOTOR_IN4_PIN,MGPIO_PIN_OUPUT);
-	MGPIO_voidSetOutputPinType(MOTOR_IN4_PORT,MOTOR_IN4_PIN,MGPIO_PIN_PUSH_PULL);
-	MGPIO_voidSetOutputPinSpeed(MOTOR_IN4_PORT,MOTOR_IN4_PIN,MGPIO_MEDIUMSPEED);
+	MGPIO_voidSetPinMode(MOTOR_IN4_PORT_PIN,MGPIO_PIN_OUPUT);
+	MGPIO_voidSetOutputPinType(MOTOR_IN4_PORT_PIN,MGPIO_PIN_PUSH_PULL);
+	MGPIO_voidSetOutputPinSpeed(MOTOR_IN4_PORT_PIN,MGPIO_MEDIUMSPEED);
 	
 }
 
 void HMOTOR_voidMoveForward(void)
 {
+	/*Motor 1 control pins (IN1,IN2) 
+	to move forward Make IN1 High and IN2 Low 
+	and reverse if backward and so is on motor 2 (IN3,IN4)*/
 	
+	/*Motor 1*/
+	MGPIO_voidSetPinValue(MOTOR_IN1_PORT_PIN,MGPIO_PIN_HIGH);
+	MGPIO_voidSetPinValue(MOTOR_IN2_PORT_PIN,MGPIO_PIN_LOW);
+	
+	/*Motor 2*/
+	MGPIO_voidSetPinValue(MOTOR_IN3_PORT_PIN,MGPIO_PIN_HIGH);
+	MGPIO_voidSetPinValue(MOTOR_IN4_PORT_PIN,MGPIO_PIN_LOW);
 }
 
 void HMOTOR_voidMoveBackward(void)
 {
+	/*Motor 1 control pins (IN1,IN2) 
+	to move forward Make IN1 High and IN2 Low 
+	and reverse if backward and so is on motor 2 (IN3,IN4)*/
 	
+	/*Motor 1*/
+	MGPIO_voidSetPinValue(MOTOR_IN1_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN2_PORT_PIN,MGPIO_PIN_HIGH);
+	
+	/*Motor 2*/
+	MGPIO_voidSetPinValue(MOTOR_IN3_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN4_PORT_PIN,MGPIO_PIN_HIGH);
 }
 
 void HMOTOR_voidStop(void)
 {
+	/*STOP All Motors*/
+	MGPIO_voidSetPinValue(MOTOR_IN1_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN2_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN3_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN4_PORT_PIN,MGPIO_PIN_LOW);
 	
 }
 
 void HMOTOR_voidMoveRight(void)
 {
+	/*Move Motors in the right (m1) Stop Motors in the left(m2)*/
 	
+	MGPIO_voidSetPinValue(MOTOR_IN1_PORT_PIN,MGPIO_PIN_HIGH);
+	MGPIO_voidSetPinValue(MOTOR_IN2_PORT_PIN,MGPIO_PIN_LOW);
+	
+	MGPIO_voidSetPinValue(MOTOR_IN3_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN4_PORT_PIN,MGPIO_PIN_LOW);
 }
 
 void HMOTOR_voidMoveLeft(void)
 {
+	/*Stop Motors in the right (m1) Move Motors in the left(m2)*/
 	
+	MGPIO_voidSetPinValue(MOTOR_IN1_PORT_PIN,MGPIO_PIN_LOW);
+	MGPIO_voidSetPinValue(MOTOR_IN2_PORT_PIN,MGPIO_PIN_LOW);
+	
+	MGPIO_voidSetPinValue(MOTOR_IN3_PORT_PIN,MGPIO_PIN_HIGH);
+	MGPIO_voidSetPinValue(MOTOR_IN4_PORT_PIN,MGPIO_PIN_LOW);
 }
 
 void HMOTOR_voidSetSpeed(u16 Copy_u16Speed)
 {
+	/*Set Compare Match Value to adjust Pwm(SPeed)*/
 	
+	MTIMER3_voidSetCompareValue(/*Timer Channel*/,Copy_u16Speed);
 }
