b750af821ae33c5aefb08c44bbf2e2ba2279adeb
a.rifatnassar@gmail.com <a.rifatnassar@gmail.com>
2023-10-20 15:30:17 +0300

    Updated usonic

--- source/HAL/HULTRASONIC/HULTRASONIC_program.c
+++ source/HAL/HULTRASONIC/HULTRASONIC_program.c
@@ -20,22 +20,23 @@
 u32 Global_u32Distance;
 static u32 Local_u32initalValue=0;
 static u32 Local_u32FinalValue=0;
+
 void HULTRASONIC_voidInit(void)
 {
-	/*Set Trigger Pin to output*/
+	/* Set Trigger Pin to output */
 	MGPIO_voidSetPinMode(ULTRASONIC_TRIGGER_PORT_PIN,MGPIO_PIN_OUPUT);
 	MGPIO_voidSetOutputPinType(ULTRASONIC_TRIGGER_PORT_PIN,MGPIO_PIN_PUSH_PULL);
 	MGPIO_voidSetOutputPinSpeed(ULTRASONIC_TRIGGER_PORT_PIN,MGPIO_MEDIUMSPEED);
 	
-	/*Set Echo Pin to altrenate function of timer2 */
+	/* Set Echo Pin to altrenate function of timer2 */
 	MGPIO_voidSetPinMode(ULTRASONIC_ECHO_PORT_PIN,MGPIO_PIN_ALTRENATE_FUNCTION);
 	MGPIO_voidSetAlternateFunction(ULTRASONIC_ECHO_PORT_PIN,1);
 }
 
 void HULTRASONIC_voidGetDistance(void)
 {
-	/*Make Trigger Pin to High for 10us the Low*/
-	/*Make Timer 2 Count From Zero*/
+	/* Make Trigger Pin to High for 10us the Low*/
+	/* Make Timer 2 Count From Zero*/
 	MTIMER2_voidClearCount();
 	MGPIO_voidSetPinValue(ULTRASONIC_TRIGGER_PORT_PIN,MGPIO_PIN_HIGH);
 	MSYSTICK_voidSetDelay(10);
@@ -47,22 +48,22 @@ void TIM2_IRQHandler(void)
 	volatile static u8 Local_u8Flag=0;
 	if(0==Local_u8Flag)
 	{
-		/*First time Echo high*/
+		/* First time Echo high */
 		Local_u32initalValue=MTIMER2_u32CaptureValue(ULTRASONIC_ECHO_CHANNEL);
 		Local_u8Flag=1;
 	}
 	else
 	{
-		/*Second time Echo Low*/
-		Local_u32FinalValue=MTIMER2_u32CaptureValue(ULTRASONIC_ECHO_CHANNEL);
+		/* Second time Echo Low */
+		Local_u32FinalValue = MTIMER2_u32CaptureValue(ULTRASONIC_ECHO_CHANNEL);
 		Local_u8Flag=0;
 		
 	}
 	
-	/*34 is speed of sound 34cm/ms=0.034 cm/us */
-	/*Distance=SpeedofSound*Time)/2 */
-	/*for ex: Timer 2 freq =8MHZ ,PreScaler=64,TickTime=64/8=8  MicroSeconnd*/
-	/*Global_u32Distance=(((0.034 * (Local_u32FinalValue-Local_u32initalValue)*8))/2);	*/
+	/* 34 is speed of sound 34cm/ms=0.034 cm/us */
+	/* Distance=SpeedofSound*Time)/2 */
+	/* for ex: Timer 2 freq =8MHZ ,PreScaler=64,TickTime=64/8=8  MicroSeconnd */
+	/* Global_u32Distance=(((0.034 * (Local_u32FinalValue-Local_u32initalValue)*8))/2);	*/
 	Global_u32Distance=(0.136 * (Local_u32FinalValue-Local_u32initalValue));
 }
 
