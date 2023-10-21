2ef7198fa57e5d26618d2c5350641fb0a4fd521b
Ahmed Samir <Ahmedsamirsalm@gmail.com>
2023-10-21 19:03:36 +0300

    ACC is Done

---
+++ include/APP/ACC/ACC_config.h
@@ -0,0 +1,28 @@
+/**********************************/
+/* Author :Ahmed Samir            */
+/* SWC :ACC		                  */
+/* Layer :APP 		              */
+/* Version :1.0                   */
+/* Date :October 21,2023          */
+/* Last Edit :N/A                 */
+/**********************************/
+
+#ifndef _ACC_CONFIG_H_
+#define _ACC_CONFIG_H_
+
+/*The max value to put on the compare value register*/
+#define ACC_MAX_SPEED			1000
+#define ACC_MIN_SPEED			100/*min speed mean that the car is stoped*/
+
+/*Distance Macros*/
+#define ACC_MAX_DISTANCE		100
+#define ACC_MIN_DISTANCE		60
+#define ACC_SAFE_DISTANCE		20
+
+/*Speed Rate change Macro*/
+#define ACC_SPEED_RATE			20
+
+
+
+
+#endif
