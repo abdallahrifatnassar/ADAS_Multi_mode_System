f9b1cdc49a89043cc53394d9d53862bec0f3b2d3
a.rifatnassar@gmail.com <a.rifatnassar@gmail.com>
2023-10-22 22:59:33 +0300

    Updateds

--- Python_Scripts/LaneKeepingAssistance.py
+++ Python_Scripts/LaneKeepingAssistance.py
@@ -1,19 +1,34 @@
 import cv2
 import numpy as np
-import config
+import utils
 
 
 def GetLaneCurve(img):
-    imgThres = config.thresholding(img)
 
-    cv2.imshow('Thres', imgThres)
-    return None
+    ### STEP 1
+    imgThres = utils.thresholding(img);
+    # cv2.imshow('Thres', imgThres);
 
-if __name__ == " __main__":
-cap = cv2.VideoCapture('vid1.mp4')
-while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (480, 240))
-    GetLaneCurve(img)
-    cv2.imshow('Vid', img)
-    cv2.waitKey(10)
+    ### STEP 2
+    h,w,c = img.shape;
+    points = utils.valTrackbars();
+    imgwarp = utils.warpImg(imgThres,points,w,h);
+    imgWrapPoint = utils.drawPoints(img, points)
+
+    cv2.imshow('Thres', imgThres);
+    cv2.imshow('Warp', imgwarp);
+    cv2.imshow('Warppoints', imgWrapPoint);
+    return None;
+
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture('vid1.mp4')
+    intialTrackBarVal = [163, 150, 129, 194];
+    utils.initializeTrackbars(intialTrackBarVal);
+    while True:
+        success, img = cap.read();
+        img = cv2.resize(img, (480, 240));
+        GetLaneCurve(img);
+        cv2.imshow('Vid', img);
+        cv2.waitKey(50);
