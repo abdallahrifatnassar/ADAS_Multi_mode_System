5a1170f63d68d3e43e095aa1458ef3e23dc63e06
Ahmed Samir <Ahmedsamirsalm@gmail.com>
2023-10-31 19:06:05 +0200

    LKS_CAMERA Demo is done to be tested in hardware

---
+++ include/APP/LKS_CAMERA/LKS_CAMERA_interface.h
@@ -0,0 +1,29 @@
+/**********************************/
+/* Author :Ahmed Samir            */
+/* SWC :LKS With Camera		      */
+/* Layer :APP 		              */
+/* Version :1.0                   */
+/* Date :October 30,2023          */
+/* Last Edit :N/A                 */
+/**********************************/
+
+#ifndef _LKS_CAMERA_INTERFACE_H_
+#define _LKS_CAMERA_INTERFACE_H_
+
+
+/*---------------------------------------------------------------------
+|  Method [LKS_CAMERA_voidMode]
+|
+|  Purpose: [This function responsible for Lane Keeping Assist Mode and Set The
+|			 Car direction according to data recieved through uart from camera ]
+|
+|  Pre-condition:Must Put Range of direction at config file first
+|
+|  Post-condition: N/A
+|
+|  Parameters:N/A
+|
+|  Returns:  N/A
+-------------------------------------------------------------------*/
+void LKS_CAMERA_voidMode(void);
+#endif 
---
+++ include/APP/LKS_CAMERA/LKS_CAMERA_private.h
@@ -0,0 +1,15 @@
+/**********************************/
+/* Author :Ahmed Samir            */
+/* SWC :LKS With Camera		      */
+/* Layer :APP 		              */
+/* Version :1.0                   */
+/* Date :October 30,2023          */
+/* Last Edit :N/A                 */
+/**********************************/
+
+#ifndef _LKS_CAMERA_PRIVATE_H_
+#define _LKS_CAMERA_PRIVATE_H_
+
+
+
+#endif
