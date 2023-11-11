02cc3e949c0c3d0af5f20350284a9c619edda5f9
elyousfy <elyousfy2000@gmail.com>
2023-11-11 15:39:56 +0200

    Last update for Raspberry Pi Code

--- Python_Scripts/utils.py
+++ Python_Scripts/utils.py
@@ -52,24 +52,24 @@ def drawPoints(img,points):
 def getHistogram(img,minper=0.1,display=False,region=1):
     if region == 1:
         histValues=np.sum(img,axis=0);
-    
+
     else:
         histValues=np.sum (img[img.shape[0]//region :,: ],axis=0)
-    
+
     # print(histValues);
     maxValue = np.max(histValues);
-    print(maxValue);
+    #print(maxValue);
     minValue = minper*maxValue;
     indexArray =np.where(histValues >= minValue); # ALL INDICES WITH MIN VALUE OR ABOVE
     basePoint =  int(np.average(indexArray)); # AVERAGE ALL MAX INDICES VALUES
-    print (basePoint);
+    #print (basePoint);
     if display:
         imgHist = np.zeros((img.shape[0],img.shape[1],3),np.uint8)
         for x,intensity in enumerate(histValues):
         # print(intensity)
             if intensity > minValue:color=(255,0,255)
             else: color=(0,0,255)
-            cv2.line(imgHist,(x,img.shape[0]),(x,img.shape[0]-(intensity//255//region)),color,1)
+            cv2.line(imgHist,(x,img.shape[0]),(x,img.shape[0]-int(intensity/255/region)),color,1)
             cv2.circle(imgHist,(basePoint,img.shape[0]),10,(0,255,255),cv2.FILLED)
         return basePoint,imgHist
     return basePoint
